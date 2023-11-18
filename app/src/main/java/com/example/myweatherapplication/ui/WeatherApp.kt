acdf15b0a353157a10ff9de5073caabdd5d6c610
Lucca Van Veerdeghem <lucca.vanveerdeghem@student.hogent.be>
2023-11-18 15:05:25 +0100

    Refactor navigation to different file

--- app/src/main/java/com/example/myweatherapplication/ui/WeatherApp.kt
+++ app/src/main/java/com/example/myweatherapplication/ui/WeatherApp.kt
@@ -2,30 +2,23 @@ package com.example.myweatherapplication.ui
 
 
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.myweatherapplication.ui.components.CurrentWeatherOverview
 import com.example.myweatherapplication.ui.components.WeatherBottomAppBar
-import com.example.myweatherapplication.ui.components.WeatherLocations
 import com.example.myweatherapplication.ui.components.WeatherTopAppBar
+import com.example.myweatherapplication.ui.navigation.Navigation
 import com.example.myweatherapplication.ui.viewModel.HomeViewModel
 
 @Composable
-fun WeatherApp(navController: NavHostController = rememberNavController(), homeViewModel: HomeViewModel = viewModel()) {
+fun WeatherApp(navController: NavHostController = rememberNavController()) {
 
-    val uiState by homeViewModel.uiState.collectAsState()
 
     val backStackEntry by navController.currentBackStackEntryAsState()
 
@@ -45,10 +38,6 @@ fun WeatherApp(navController: NavHostController = rememberNavController(), homeV
 
     val goToList = { navController.navigate(WeatherOverviewScreen.List.name) }
 
-    val goToClickedLocation: (locatie:String)-> Unit = {
-        homeViewModel.setChosenLocation(it)
-        navController.navigate(WeatherOverviewScreen.Detail.name)
-    }
 
     Scaffold(
         containerColor = Color.Transparent,
@@ -65,21 +54,7 @@ fun WeatherApp(navController: NavHostController = rememberNavController(), homeV
 
     ) { innerPadding ->
 //todo: bring to different component
-        NavHost(
-            navController = navController,
-            startDestination = WeatherOverviewScreen.Start.name,
-            modifier = Modifier.padding(innerPadding),
-        ) {
-            composable(route = WeatherOverviewScreen.Start.name) {
-                CurrentWeatherOverview(modifier = Modifier)
-            }
-            composable(route = WeatherOverviewScreen.List.name) {
-                WeatherLocations(modifier = Modifier, goToClickedLocation)
-            }
-            composable(route = WeatherOverviewScreen.Detail.name){
-                CurrentWeatherOverview(modifier = Modifier, location = uiState.chosenLocation)
-            }
-        }
+        Navigation(navController, innerPadding)
     }
 }
 
---
+++ app/src/main/java/com/example/myweatherapplication/ui/navigation/Navigation.kt
@@ -0,0 +1,42 @@
+package com.example.myweatherapplication.ui.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.myweatherapplication.ui.WeatherOverviewScreen
+import com.example.myweatherapplication.ui.components.CurrentWeatherOverview
+import com.example.myweatherapplication.ui.components.WeatherLocations
+import com.example.myweatherapplication.ui.viewModel.HomeViewModel
+
+@Composable
+fun Navigation(navController: NavHostController, innerPadding:PaddingValues, homeViewModel: HomeViewModel = viewModel()){
+    val uiState by homeViewModel.uiState.collectAsState()
+    val goToClickedLocation: (locatie:String)-> Unit = {
+        homeViewModel.setChosenLocation(it)
+        navController.navigate(WeatherOverviewScreen.Detail.name)
+    }
+
+
+    NavHost(
+        navController = navController,
+        startDestination = WeatherOverviewScreen.Start.name,
+        modifier = androidx.compose.ui.Modifier.padding(innerPadding),
+    ) {
+        composable(route = WeatherOverviewScreen.Start.name) {
+            CurrentWeatherOverview(modifier = Modifier)
+        }
+        composable(route = WeatherOverviewScreen.List.name) {
+            WeatherLocations(modifier = Modifier, goToClickedLocation)
+        }
+        composable(route = WeatherOverviewScreen.Detail.name){
+            CurrentWeatherOverview(modifier = Modifier, location = uiState.chosenLocation)
+        }
+    }
+}
