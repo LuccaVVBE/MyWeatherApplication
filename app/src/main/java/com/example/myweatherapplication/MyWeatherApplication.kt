76c2186acd6e23cfb6d5a8c70536acfddfa6a4e1
Lucca Van Veerdeghem <lucca.vanveerdeghem@student.hogent.be>
2023-11-18 10:14:54 +0100

    Start fix

---
+++ app/src/main/java/com/example/myweatherapplication/MyWeatherApplication.kt
@@ -0,0 +1,14 @@
+package com.example.myweatherapplication
+//
+//import android.app.Application
+//import com.example.myweatherapplication.data.AppContainer
+//import com.example.myweatherapplication.data.DefaultAppContainer
+//
+//class MyWeatherApplication : Application() {
+//    lateinit var container: AppContainer
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        container = DefaultAppContainer()
+//    }
+//}
